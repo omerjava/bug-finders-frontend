23a0b6e1d602ea7f23d84e0346124de5d80912d0
omerjava <ofk45@hotmail.com>
2022-07-14 21:14:23 +0200

    update

--- src/config/config.js
+++ src/config/config.js
@@ -1,2 +1,2 @@
 // export const ORIGIN = "http://localhost:5000";
-export const ORIGIN = "https://bugfinders-backend.herokuapp.com/";
+export const ORIGIN = "https://bugfinders-backend.herokuapp.com";
